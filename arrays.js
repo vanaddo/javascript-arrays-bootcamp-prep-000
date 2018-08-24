0186289419c25b0c29384d0cad3b638e379a725f
vanaddo@gmail.com <vanaddo@gmail.com>
2018-08-24 02:22:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
+
+
+function addElementToBeginningOfArray() {}
+function destructiveAddElementToBeginningOfArray() {}
+
+function addElementToEnd() {}
+function destructiveAddElementToEndOfArray() {}
+function accessElementInArray() {}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  return chocolateBars.shift()
+}
